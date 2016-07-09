7bad7807241c02d7f29a92566876333335674fd3
Alexey Kulakov <uni_que@me.com>
2016-07-09 05:58:21 -0700

    corrections to ember build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,26 +1,30 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
-var env = EmberApp.env();
-var isProductionLikeBuild = ['production', 'staging', 'development'].indexOf(env) > -1;
+// var env = EmberApp.env();
+// var isProductionLikeBuild = ['production', 'staging', 'development'].indexOf(env) > -1;
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    fingerprint: {
-      enabled: isProductionLikeBuild
-    },
     sourcemaps: {
-      enabled: !isProductionLikeBuild,
-    },
-    minifyCSS: {
-      enabled: isProductionLikeBuild
-    },
-    minifyJS: {
-      enabled: isProductionLikeBuild
-    },
-
-    tests: process.env.EMBER_CLI_TEST_COMMAND || !isProductionLikeBuild,
-    hinting: process.env.EMBER_CLI_TEST_COMMAND || !isProductionLikeBuild,
+      enabled: true,
+      extensions: ['js']
+    }
+    // fingerprint: {
+    //   enabled: isProductionLikeBuild
+    // },
+    // sourcemaps: {
+    //   enabled: !isProductionLikeBuild,
+    // },
+    // minifyCSS: {
+    //   enabled: isProductionLikeBuild
+    // },
+    // minifyJS: {
+    //   enabled: isProductionLikeBuild
+    // },
+    //
+    // tests: process.env.EMBER_CLI_TEST_COMMAND || !isProductionLikeBuild,
+    // hinting: process.env.EMBER_CLI_TEST_COMMAND || !isProductionLikeBuild,
   });
 
   // Use `app.import` to add additional libraries to the generated
